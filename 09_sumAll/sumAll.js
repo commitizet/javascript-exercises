8f984f5c99997066043ca9780d04931b9a91fde3
commitizet <217013521+commitizet@users.noreply.github.com>
2025-09-17 07:34:46 +0800

    complete the sumAll function

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function(...nums) {
-    if (nums.some(num => typeof num !== 'number' || num < 0 || (num+"").split('').includes('.'))) return 'ERROR';
+const sumAll = function (...nums) {
+  if (
+    nums.some(
+      (num) =>
+        typeof num !== "number" || num < 0 || (num + "").split("").includes(".")
+    )
+  )
+    return "ERROR";
+
+  let arr = [];
+  if (nums[0] > nums[1]) {
+    for (let i = nums[0]; i >= nums[1]; i--) {
+      arr.push(i);
+    }
+  } else {
+    for (let i = nums[0]; i <= nums[1]; i++) {
+      arr.push(i);
+    }
+  }
+
+  return arr.reduce((acc, curr) => acc + curr, 0);
 };
 
 // Do not edit below this line
