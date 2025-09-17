bbb26b028514f480bd990ee2f094922e2fc7f90d
commitizet <217013521+commitizet@users.noreply.github.com>
2025-09-17 08:46:45 +0800

    solve the convertToCelsius function

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  // C = 5/9(F-32)
+  let celsius = 5/9 * (num - 32);
+  return (Number.isInteger(celsius)) ? celsius : +celsius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  
 };
 
 // Do not edit below this line
