e7d8b4a9adc15084e47497d0e856e2271e201c8b
commitizet <217013521+commitizet@users.noreply.github.com>
2025-09-17 09:43:15 +0800

    solve the palindrome function

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const lowerStr = str.toLowerCase();
+    const arrStr = lowerStr.split('');
+    const filteredArr = arrStr.filter(el => el.match(/[a-z0-9]/));
+    const cleanedStr = filteredArr.join('');
+    const reversedStr = filteredArr.reverse().join('');
+    return (cleanedStr === reversedStr) ? true : false;
 };
 
 // Do not edit below this line
