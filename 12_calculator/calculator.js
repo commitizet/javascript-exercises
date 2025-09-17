b6600e39c1e78ced3d387e49201023fc8ea8d4c8
commitizet <217013521+commitizet@users.noreply.github.com>
2025-09-17 09:04:24 +0800

    solve the add function

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,20 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
 };
 
 // Do not edit below this line
