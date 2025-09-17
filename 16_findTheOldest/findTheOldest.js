bf79bd4b0036731e55b7d57eaebdc825915665b9
commitizet <217013521+commitizet@users.noreply.github.com>
2025-09-17 11:48:07 +0800

    solve the findTheOldest function

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  let d = new Date();
+  let year = d.getFullYear();
+  let arrWithAge = arr.map((obj) => {
+    let age = obj.yearOfDeath
+      ? obj.yearOfDeath - obj.yearOfBirth
+      : year - obj.yearOfBirth;
+    return {
+      ...obj,
+      age,
+    };
+  });
 
+  let oldestAge = arrWithAge.reduce(
+    (max, curr) => (curr.age > max ? (max = curr.age) : (max = max)),
+    0
+  );
+
+  return arrWithAge.find((obj) => obj.age === oldestAge);
 };
 
 // Do not edit below this line
