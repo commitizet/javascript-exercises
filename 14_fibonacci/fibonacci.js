4cc738dc071999b1b696f5c00547296ebcdea377
commitizet <217013521+commitizet@users.noreply.github.com>
2025-09-17 14:50:20 +0800

    solve the fibonacci function

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let n = +num;
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
 
+    let x = 0;
+    let y = 1;
+    let z;
+    for (let i = 1; i <= (n - 1); i++){
+        z = x + y;
+        x = y;
+        y = z;
+    }
+    
+    return y;
 };
 
 // Do not edit below this line
