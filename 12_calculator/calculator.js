c1c75826ac8d641701be98c0787adfb7055aeb54
commitizet <217013521+commitizet@users.noreply.github.com>
2025-09-17 09:14:10 +0800

    modify the factorial function

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -21,12 +21,12 @@ const power = function(num1, num2) {
 const factorial = function(num) {
   if (num === 0) return 1;
 
-  let arr = [];
-  for (let i = 1; i <= num; i++){
-    arr.push(i);
+  let result = 1;
+  for (let i = num; i > 0; i--){
+    result *= i;
   }
 
-  return multiply(arr);
+  return result;
 };
 
 // Do not edit below this line
